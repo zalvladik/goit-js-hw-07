56ddb15e16238bd0173f74e3924006f05f07b9b5
zalvladik <zalipkav@ukr.net>
2023-03-07 17:47:37 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,25 +7,20 @@ galleryContainer.addEventListener('click',stopDefAction)
 function createGallery(evt) {
     return evt.map(({preview, original, description}) => {
         return`
-        <a 
-        class="gallery__item" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="${description}" />
-    </a> 
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" /> </a> 
         `
     }).join('')
 }
-
 
 function stopDefAction(evt) {
     evt.preventDefault();
     if (evt.target.nodeName !== "IMG") {
         return console.log('missClick')
     };
-    
     var lightbox = new SimpleLightbox('.gallery a', 
     {captions: true, 
      captionDelay: 250 , 
      captionsData : 'alt', 
     });
-   
 }
